c46a7ee2142766f8693e2ad3546afe6984505f3b
Ben Clare <ben.clare2@gmail.com>
2019-05-07 21:23:03 +1000

    Init commit

---
+++ src/main/java/com/clare/ben/services/news/collectors/redditnewsfetcher/RedditNewsFetcherApplication.java
@@ -0,0 +1,13 @@
+package com.clare.ben.services.news.collectors.redditnewsfetcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedditNewsFetcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedditNewsFetcherApplication.class, args);
+    }
+
+}
